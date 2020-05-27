ecd7420ebfb1a88f84b7faaeffb426caf2ddf268
anitakurm <anitakurm@gmail.com>
2020-05-27 05:56:01 +0200

    aesthetic changes + upload data for replicability

--- twitterQA_bert_battle.py
+++ twitterQA_bert_battle.py
@@ -29,7 +29,6 @@ for file_name in filenames:
     # append data from every file to one large dataframe
     big_df = big_df.append(df, ignore_index=True)
 
-
 # Load both models, track loading time, make a model list
 start = time.time()
 bert = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
@@ -74,7 +73,8 @@ def bert_QA(row):
                                                     skip_special_tokens=True)
     # Combine the tokens in the answer and print it out.
     row[answer_id] = tokenizer.convert_tokens_to_string(answer_tokens)
-    # track time from model loading to answer
+    
+    # track time from application of tokenizer to getting the answer
     row[time_id] = str(time.time() - start)
     return row
 
@@ -101,7 +101,7 @@ def distil_bert_QA(row):
 
     # Combine the tokens in the answer and write it down
     row[answer_id] = tokenizer.convert_tokens_to_string(answer_tokens)
-    # track time from model loading to answer
+    # track time from application of tokenizer to getting the answer
     row[time_id] = str(time.time() - start)
     return row
 
@@ -123,11 +123,13 @@ for m_name, model in model_dict.items():
         # Run BERT QA on data, track time
         big_df = big_df.apply(bert_QA, axis=1)
 
+# Write tokenizer loading times down
 tokenizer_loading_times = {
     "L_BERT": str(bert_tok_time),
     "DistilBERT": str(distilbert_tok_time)
 }
 
+# Write models’ inference and inference time outputs down
 big_df.to_csv('twitterQA_berts.csv')
 
 with open('model_loading.txt', 'w') as file:
@@ -135,69 +137,3 @@ with open('model_loading.txt', 'w') as file:
 
 with open('tokenizer_loading.txt', 'w') as file:
     file.write(json.dumps(tokenizer_loading_times))
-
-# # -------------------- DRAFTS:
-
-# # A signle example
-# tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-
-# question = "How many parameters does BERT-large have?"
-# answer_text = "BERT-large is really big... it has 24-layers and an embedding size of 1,024, for a total of 340M parameters! Altogether it is 1.34GB, so expect it to take a couple minutes to download to your Colab instance."
-
-# # Apply the tokenizer to the input text, treating them as a text-pair.
-# input_ids = tokenizer.encode(question, answer_text)
-
-# print('The input has a total of {:} tokens.'.format(len(input_ids)))
-
-# # BERT only needs the token IDs, but for the purpose of inspecting the 
-# # tokenizer's behavior, let's also get the token strings and display them.
-# tokens = tokenizer.convert_ids_to_tokens(input_ids)
-
-# # For each token and its id...
-# for token, id in zip(tokens, input_ids):
-    
-#     # If this is the [SEP] token, add some space around it to make it stand out.
-#     if id == tokenizer.sep_token_id:
-#         print('')
-    
-#     # Print the token string and its ID in two columns.
-#     print('{:<12} {:>6,}'.format(token, id))
-
-#     if id == tokenizer.sep_token_id:
-#         print('')
-
-# start_scores, end_scores = model(torch.tensor([input_ids]))
-# # Find the tokens with the highest `start` and `end` scores.
-# answer_start = torch.argmax(start_scores)
-# answer_end = torch.argmax(end_scores)
-
-# # Combine the tokens in the answer and print it out.
-# answer = ' '.join(tokens[answer_start:answer_end+1])
-
-# print('Answer: "' + answer + '"')
-
-# # Per row
-
-# # Define util function to run diff versions of BERT
-# def bert_QA(row, distil=False):
-#     context = row['Tweet']      # get context (tweet text)
-#     question = row['Question']  # get question
-#     start = time.time()         # start tracking time
-#     if distil:                  # choose version of BERT
-#         model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-#     else:
-#         model = 
-#     answer =                    # get model's answer
-#     run_time =                  # track time from model loading to answer
-#     return answer, run_time
-
-
-# # Run BERT QA on data, track time
-# df['BERT_answer'], df['BERT_time'] = df.apply(bert_QA,
-#                                               args=(distil=False),
-#                                               axis=1)
-
-# df['dist_answer'], df['dist_time'] = df.apply(bert_QA,
-#                                               args=(distil=True),
-#                                               axis=1)
-
